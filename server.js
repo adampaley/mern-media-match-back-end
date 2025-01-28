42a23ffb551bcc3b44442e6e0c69d838aa2149e2
Adam Paley <apaley95@gmail.com>
2025-01-28 14:38:28 -0500

    transformed genre from numeric api value to semantic name

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express()
 const mongoose = require('mongoose')
 const logger = require('morgan')
 const cors = require('cors')
+const { mapGameGenres } = require('./utility/video-games/utility.js')
 
 // connect to DB
 mongoose.connect(process.env.MONGODB_URI)
@@ -30,7 +31,7 @@ app.post('/', async (req, res) => {
             'Content-Type': 'text/plain'
         }
         
-        const body = 'fields *; limit 500; exclude alternative_names, category, checksum, external_games, game_localizations, game_modes, involved_companies, keywords, platforms, player_perspectives, release_dates, similar_games, tags, themes, updated_at;'
+        const body = 'fields *; limit 500; exclude alternative_names, category, created_at, checksum, dlcs, external_games, game_localizations, game_modes, involved_companies, keywords, platforms, player_perspectives, release_dates, similar_games, tags, themes, updated_at;'
         
         const apiRes = await fetch(BASE_URL, {
             method: 'POST',
@@ -42,7 +43,13 @@ app.post('/', async (req, res) => {
             throw new Error(`API Request failed with status: ${apiRes.status}`)
         }
         const data = await apiRes.json()
-        res.status(200).json(data)
+
+        const transformedData = data.map(game => ({
+            ...game,
+            genres: Array.isArray(game.genres) ? mapGameGenres(game.genres) : [],
+        }))
+
+        res.status(200).json(transformedData)
       } catch (err) {
         console.log('Error fetching data from API:', err.message)
         res.status(500).json({ message: err.message })
