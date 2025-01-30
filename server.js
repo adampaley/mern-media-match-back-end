3f594e89145cac284eb0a29ff976e4e8ff99b371
Ryan Deist <ryandeist@gmail.com>
2025-01-30 12:56:08 -0500

    misc commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express()
 const mongoose = require('mongoose')
 const logger = require('morgan')
 const cors = require('cors')
-const {   
-    dateOfRelease,
-    generateRandomOffset,
-    generateRandomPrice,
-    mapGameGenres,
+const {
+  dateOfRelease,
+  generateRandomOffset,
+  generateRandomPrice,
+  mapGameGenres,
 } = require('./utility/video-games/utility.js')
 //const testJwtRouter = require('./controllers/test-jwt.js');
 const authRouter = require('./controllers/auth.js');
@@ -30,60 +30,60 @@ app.use(cors())
 
 // routes
 app.post('/', async (req, res) => {
-    
-      try {
-        const BASE_URL = `https://api.igdb.com/v4/games`
-        
-        const headers = {
-            'Client-ID': `${process.env.CLIENT_ID}`,
-            'Authorization': `Bearer ${process.env.API_KEY}`,            
-            'Content-Type': 'text/plain'
-        }
-        
-        let genreNames = ''
-        
-        if (req.query) {
-            genreNames = req.query.genres
-        }
 
-        const body = `fields age_ratings.rating, artworks, cover.image_id, first_release_date, genres.name, name, screenshots, slug, storyline, summary, total_rating,  url;  limit 2; offset ${generateRandomOffset()};`
-        
-        let newBody = body
-        
-        if (genreNames && genreNames.length > 0) {
-           newBody += ` where genres = (${mapGameGenres([genreNames.split(',')]).join(',')});`
-        }
-        
-        const apiRes = await fetch(BASE_URL, {
-            method: 'POST',
-            headers: headers,
-            body: newBody
-        })
+  try {
+    const BASE_URL = `https://api.igdb.com/v4/games`
 
-        if (!apiRes.ok) {
-            throw new Error(`API Request failed with status: ${apiRes.status}`)
-        }
-        const data = await apiRes.json()
+    const headers = {
+      'Client-ID': `${process.env.CLIENT_ID}`,
+      'Authorization': `Bearer ${process.env.API_KEY}`,
+      'Content-Type': 'text/plain'
+    }
 
-        const transformedData = data.map(game => ({
-            ...game,
-            cover: `images.igdb.com/igdb/image/upload/t_cover_big/${game.cover?.image_id}.jpg`,
-            genres: game.genres ?? [],
-            title: game.name,
-            media: 'Video Games',
-            parentalRating: game.age_ratings ? `PEGI ${game.age_ratings[0].rating}` : 'Not Rated',
-            price: generateRandomPrice(),
-            releaseDate: game.first_release_date ? dateOfRelease(game.first_release_date) : null,
-            storyline: game.storyline ? game.storyline : '',
-            summary: game.summary ? game.summary : '',
-            totalRating: Math.round(game.total_rating)
-        }))
+    let genreNames = ''
 
-        res.status(200).json(transformedData)
-      } catch (err) {
-        console.log('Error fetching data from API:', err.message)
-        res.status(500).json({ message: err.message })
-      }
+    if (req.query) {
+      genreNames = req.query.genres
+    }
+
+    const body = `fields age_ratings.rating, artworks, cover.image_id, first_release_date, genres.name, name, screenshots, slug, storyline, summary, total_rating,  url;  limit 2; offset ${generateRandomOffset()};`
+
+    let newBody = body
+
+    if (genreNames && genreNames.length > 0) {
+      newBody += ` where genres = (${mapGameGenres([genreNames.split(',')]).join(',')});`
+    }
+
+    const apiRes = await fetch(BASE_URL, {
+      method: 'POST',
+      headers: headers,
+      body: newBody
+    })
+
+    if (!apiRes.ok) {
+      throw new Error(`API Request failed with status: ${apiRes.status}`)
+    }
+    const data = await apiRes.json()
+
+    const transformedData = data.map(game => ({
+      ...game,
+      cover: `images.igdb.com/igdb/image/upload/t_cover_big/${game.cover?.image_id}.jpg`,
+      genres: game.genres ?? [],
+      title: game.name,
+      media: 'Video Games',
+      parentalRating: game.age_ratings ? `PEGI ${game.age_ratings[0].rating}` : 'Not Rated',
+      price: generateRandomPrice(),
+      releaseDate: game.first_release_date ? dateOfRelease(game.first_release_date) : null,
+      storyline: game.storyline ? game.storyline : '',
+      summary: game.summary ? game.summary : '',
+      totalRating: Math.round(game.total_rating)
+    }))
+
+    res.status(200).json(transformedData)
+  } catch (err) {
+    console.log('Error fetching data from API:', err.message)
+    res.status(500).json({ message: err.message })
+  }
 })
 
 // controllers
