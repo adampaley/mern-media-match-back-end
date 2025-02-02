f40e8d4466a2a3ab6d0c8f72cf6a60027224e51e
Ryan Deist <ryandeist@gmail.com>
2025-02-01 21:39:46 -0500

    cart index and delete up and running on the backend

--- controllers/carts.js
+++ controllers/carts.js
@@ -11,11 +11,11 @@ const { Cart, User } = require('../models/user')
 // GET /carts
 router.get('/', verifyToken, async (req, res) => {
     try {
-        if (req.user._id !== req.body.userId) {
+        if (req.user._id !== req.headers.userid) {
             return res.status(403).json({ err: "Unauthorized" })
         }
 
-        const user = await User.findById(req.body.userId) 
+        const user = await User.findById(req.headers.userid) 
 
         if (!user) {
             return res.status(404).json({ err: 'User not found.' })
@@ -52,6 +52,18 @@ router.post('/', verifyToken, async (req, res) => {
         await user.save()
         
         res.status(200).json({ cart: user.cart})
+    } catch (err) {
+        res.status(500).json({ err: err.message })
+    }
+})
+
+router.delete('/:cartItemId', verifyToken, async (req,res) => {
+    try {
+        const user = await User.findById(req.headers.userid)
+        const cartItem = user.cart.id(req.params.cartItemId)
+        user.cart.remove({ _id: req.params.cartItemId})
+        await user.save()
+        res.status(200).json({message: 'cart item deleted'})
     } catch (err) {
         res.status(500).json({ err: err.message })
     }
